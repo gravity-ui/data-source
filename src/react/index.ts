f7b060b4d51a7de2f7767110f5595a6ed874e018
Mikhail Golbakh <golbah@yandex.ru>
2024-08-02 12:34:33 +0300

    fix(react): add exports for error props

--- src/react/index.ts
+++ src/react/index.ts
@@ -3,5 +3,6 @@ export {DataManagerContext, useDataManager} from './DataManagerContext';
 export type {WithDataManagerProps} from './withDataManager';
 export {withDataManager} from './withDataManager';
 
+export type {ErrorAction, ErrorViewProps} from './components/types';
 export * from './components/DataLoader';
 export * from './components/DataInfiniteLoader';
