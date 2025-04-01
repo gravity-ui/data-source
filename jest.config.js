6503ad77a8b234d7499589e98f8d09d532930603
Mikhail Golbakh <golbah@yandex.ru>
2025-04-01 13:26:31 +0200

    test(react): add tests for DataManager

--- jest.config.js
+++ jest.config.js
@@ -1,7 +1,9 @@
 module.exports = {
     roots: ['<rootDir>/src'],
-    testMatch: ['**/__tests__/*.test.ts'],
+    testMatch: ['**/__tests__/*.test.ts{,x}'],
     transform: {
         '^.+\\.tsx?$': '@swc/jest',
     },
+    testEnvironment: 'jsdom',
+    setupFilesAfterEnv: ['<rootDir>/src/setupTests.ts'],
 };
