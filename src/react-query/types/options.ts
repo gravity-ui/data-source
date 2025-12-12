2cd85f3713385b59e534711aaad6e3796d08ae5b
NasgulNexus <mostovoyegor@yandex-team.ru>
2025-12-12 12:25:56 +0100

    fix: revert commit

--- src/react-query/types/options.ts
+++ src/react-query/types/options.ts
@@ -1,5 +1,7 @@
 import type {DefaultError, QueryKey} from '@tanstack/react-query';
 
+import type {OptimisticConfig} from '../../core/types/Normalizer';
+
 import type {RefetchInterval} from './refetch-interval';
 
 export interface QueryDataAdditionalOptions<
@@ -9,4 +11,10 @@ export interface QueryDataAdditionalOptions<
     TQueryKey extends QueryKey = QueryKey,
 > {
     refetchInterval?: RefetchInterval<TQueryFnData, TError, TQueryData, TQueryKey>;
+    /** Normalization configuration (enable/disable) */
+    normalize?: boolean;
+    /** Optimistic data update configuration */
+    optimistic?: boolean | OptimisticConfig;
+    /** Invalidate data configuration */
+    invalidate?: boolean;
 }
