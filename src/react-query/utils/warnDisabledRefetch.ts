87a15b3ad29bd5762560f09a3ea96918c49b05f4
NasgulNexus <mostovoyegor@yandex-team.ru>
2025-12-22 11:42:02 +0100

    fix: add types

--- src/react-query/utils/warnDisabledRefetch.ts
+++ src/react-query/utils/warnDisabledRefetch.ts
@@ -1,5 +1,5 @@
 import {warn} from './warn';
 
-export const warnDisabledRefetch = () => {
+export const warnDisabledRefetch = async (): Promise<void> => {
     warn('Disabled refetch is called');
 };
